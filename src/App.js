177c50251dafe768808e7a44fb06b042d3e2f56e
Altamash <altamashaseer9@gmail.com>
2022-09-16 23:54:50 +0530

    adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/NavBar';
 import News from './components/News';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar/>    {/* camel case mandetory */}
-        <News pageSize='6' country='in' category='science' />
-      </div>
+      <>
+        <BrowserRouter>
+          <Navbar /> {/* camel case mandetory */}
+          <Routes>
+            <Route path="/" element={<News pageSize='6' country='in' category='science' />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     )
   }
 }
